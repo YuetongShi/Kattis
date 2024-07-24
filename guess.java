f06b913b474905ed38f8775b2e0e19a7db2a3ac5
Yuetong Shi <162837217+YuetongShi@users.noreply.github.com>
2024-07-24 17:34:46 +0800

    Create guess.java

---
+++ guess.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class guess {
+    private static int lower = 1;
+    private static int higher = 1000;
+
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++){
+            int mid = (lower + higher)/ 2;
+            System.out.println(mid);
+            Scanner s = new Scanner(System.in);
+
+            String result = s.nextLine();
+
+            if (result.equals("correct"))
+                break;
+            else if (result.equals("higher"))
+                lower = mid;
+            else
+                higher = mid;
+        }
+    }
+}
