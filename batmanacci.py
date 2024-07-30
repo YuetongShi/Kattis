f5d06659c71005e703b3d0704e0ac9916229a5e2
Yuetong Shi <162837217+YuetongShi@users.noreply.github.com>
2024-07-30 09:50:28 +0800

    Update batmanacci.py

--- batmanacci.py
+++ batmanacci.py
@@ -3,19 +3,19 @@ data = data.split()
 N = int(data[0])
 K = int(data[1])
 
-bat = [0,1]
-for i in range(2, N+1):
+bat = [0, 1]
+for i in range(2, N + 1):
     fib.append(bat[-1] + bat[-2])   #bat[-1] and bat[-2] are subcases
 
 while N > 2:
-    if K > bat[n-2]:  #remove the first part of bat[n]
-        K -= fib[n-2]  #update the position of K
+    if K > bat[n - 2]:  #remove the first part of bat[n]
+        K -= bat[n - 2]  #update the position of K
         N -= 1
     else:
         N -= 2
 
-if n == 2:
+if N == 2:
     print("A")
 
-if n == 1:
+if N == 1:
     print("N")
